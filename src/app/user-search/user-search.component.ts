b44f13a7da9d60bef43f1d9ed51a47c9be1fd9ce
Leonardo Santos Severino <leonardossev@gmail.com>
2019-04-14 19:06:51 -0300

    Uses the userSearchService as a provider for fetching the user's information

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 
+import { UserSearchService } from './user-search.service';
+
 @Component({
   selector: 'user-search',
   templateUrl: './user-search.component.html',
-  styleUrls: ['./user-search.component.css']
+  styleUrls: ['./user-search.component.css'],
+  providers: [UserSearchService]
 })
 export class UserSearchComponent {
 
   username: string = '';
 
+  userSearchService: UserSearchService;
+
+  constructor (userSearchService: UserSearchService) {
+    this.userSearchService = userSearchService;
+  }
+
   searchUser () {
-    //TODO: fetch user information.
+    this.userSearchService.getUserDetails(this.username)
+    .subscribe(response => {
+      // TODO: perform something with the response.
+    },
+    error => {
+      // TODO: perform something with the error response.
+    });
   }
 }
